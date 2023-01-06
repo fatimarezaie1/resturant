532a900e4d11eb7ee5d642338d318f8880bdc360
Fatima <fatimarezaie705@gmail.com>
2023-01-06 16:51:13 +0430

    resturant

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: ["pepperoni", "mushroom", "mozarella"],
         id: 0,
         price: 14,
         emoji: "🍕"
@@ -13,7 +13,7 @@ const menuArray = [
         emoji: "🍔",
         id: 1
     },
-        {
+    {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
@@ -21,3 +21,59 @@ const menuArray = [
         id: 2
     }
 ]
+
+let main = document.getElementById("main");
+for(let i=0; i<menuArray.length; i++){
+        main.innerHTML += `
+        <div class="pizza">
+            <div class="order-photo">${menuArray[i].emoji} </div>
+            <div class="order-description">
+                <span class="name">${menuArray[i].name}</span>
+                <span class="ingredients">${menuArray[i].ingredients}</span>
+                <span class="price"> $ ${Number(menuArray[i].price)}</span>
+            </div>
+            <div class="btn">
+            <button id=${"addbtn" + i } class="addbtn" onclick = "addOrder(${i})" onclick = "priceCollection(${i})"}>+</button>
+            </div>
+        </div> 
+        <hr class="line">`
+}
+
+let order = document.getElementById("orderList");
+function addOrder (i){
+    order.innerHTML += `
+    <ul>
+        <li id='ordername'> ${menuArray[i].name}</li>
+        <li class=${"price"} id=${price}  > $ ${Number(menuArray[i].price)}</li>
+    </ul>
+  `
+}
+
+
+
+// let price = document.getElementById("price");
+// let allprice ;
+// allprice +=price ;
+// let collection = document.getElementById("collection");
+//     collection.innerHTML = `<ul>
+//     <li style="font-size: larger; font-weight: 600;" >Total Price: </li>
+//     <li class="total"> ${allprice}</li>
+//     </ul>`
+
+
+
+
+
+let footer = document.getElementById("footer");
+let popUp = document.getElementById("popup");
+let finalDivs = document.getElementById("finaldivs");
+
+function parOrder(){
+    popUp.classList.add("open-form");
+}
+function completeOrder(){
+    popUp.classList.remove("open-form");
+    footer. innerHTML = `
+        <div class = "final-div"> Thanks, James! Your order is on its way!</div>
+    `
+}
